1f29f5c572bfc301bc064e3e23e425ec2322b09b
huitson-pereira <124617896+huitson-pereira@users.noreply.github.com>
2023-10-29 16:05:19 -0300

    feat: Criação do arquivo unidadeo.routes.js e adicionda a rota no arquivo index.js da pasta routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const { routesFromUsuario } = require('./usuario.routes');
+const { routesFromUnidade } = require('./unidade.route');
 
 const routes = Router();
 
-routes.use('/api', [routesFromUsuario()]);
+routes.use('/api', [routesFromUsuario(), routesFromUnidade()]);
 
 module.exports = routes;
